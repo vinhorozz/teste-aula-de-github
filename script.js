864c05736939f9c680ba9bf10f3b41fb34edf454
Flávio Roberto Ferreira de Queiroz <144744913+vinhorozz@users.noreply.github.com>
2025-08-17 14:29:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ import {turboLogger} from "./TurboLogger.js";
 console.log("Estou usando o gitHub");
 
 turboLogger("Estou usando aprendenedo o fluxo do Git",'red');
+
+turboLogger("Alteração realizado no repositório remoto",'white');
