414f493cfb7c8238b8452a109dff6818da744433
Flávio Queiroz <vinhorozz@hotmail.com>
2025-08-17 13:31:55 -0300

    Atualizadas as mensagens em versão teste

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ import {turboLogger} from "./TurboLogger.js";
 console.log("Estou usando o gitHub");
 
 turboLogger("Estou usando aprendenedo o fluxo do Git",'red');
+
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!","darkgreen")
